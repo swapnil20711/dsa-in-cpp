91df761e3e1f1957352f3d8f0ec731d3a1fc7edc
swapnil20711 <swapnil.bhojwani@gmail.com>
2026-08-05 01:03:42 +0530

    feat: print all divisors done

---
+++ printAllDivisors.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printAllDivisors(int n){
+    for (int i = 1; i <=n; i++)
+    {
+        if(n%i==0){
+            cout<<i<< " ";
+        }
+    }
+    
+}
+
+void printAllDivisors2(int n){
+    for (int i = 1; i <=sqrt(n); i++)
+    {
+        if(n%i==0){
+            cout<<i<< " ";
+            if((n/i)!=i){
+                cout<<(n/i)<<" ";
+            }
+        }
+    }
+    
+}
+
+int main()
+{
+    int number;
+
+    cin >> number;
+    printAllDivisors2(number);
+}
