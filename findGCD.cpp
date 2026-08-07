63bedeb80944cd2f4b03b5727dd42b0b7709a406
swapnil20711 <swapnil.bhojwani@gmail.com>
2026-08-07 09:56:30 +0530

    find gcd optimal approach

--- findGCD.cpp
+++ findGCD.cpp
@@ -16,10 +16,26 @@ int findGCD(int n1, int n2)
     return gcd;
 }
 
+int findGCDOptimal(int n1, int n2)
+{
+    int gcd = 1;
+    int minNumber = min(n1, n2);
+
+    for (int i = minNumber; i >=1 ; i--)
+    {
+        if (n1 % i == 0 && n2 % i == 0)
+        {
+            gcd = i;
+            break;
+        }
+    }
+    return gcd;
+}
+
 int main()
 {
     int n1, n2;
     cin >> n1 >> n2;
 
-    cout << "GCD of " << n1 << " and " << n2 << " is : " << findGCD(n1, n2);
+    cout << "GCD of " << n1 << " and " << n2 << " is : " << findGCDOptimal(n1, n2);
 }
