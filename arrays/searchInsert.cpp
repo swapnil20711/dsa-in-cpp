536b87e835770f2a61f7f256743f6e246057e6db
swapnil20711 <swapnil.bhojwani@gmail.com>
2026-09-09 22:46:04 +0530

    fix: search insert problem solved

--- arrays/searchInsert.cpp
+++ arrays/searchInsert.cpp
@@ -1,27 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int searchInsert(vector<int>& nums, int target) {
-        int left = 0;
-        int right = nums.size() - 1;
+// int searchInsert(vector<int>& nums, int target) {
+//         int left = 0;
+//         int right = nums.size() - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;  
+//         while (left <= right) {
+//             int mid = left + (right - left) / 2;
 
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+//             if (nums[mid] == target) {
+//                 return mid;
+//             } else if (nums[mid] < target) {
+//                 left = mid + 1;
+//             } else {
+//                 right = mid - 1;
+//             }
+//         }
+//         return left;
+//     }
+
+int searchInsert(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    int low = 0, high = n - 1;
+    while (low <= high)
+    {
+        int mid = low + (high - low) / 2;
+        if (nums[mid] >= target)
+        {
+            high = mid - 1;
         }
-        return left;
+        else
+            low = mid + 1;
     }
+    return low;
+}
 
 int main()
 {
-    vector<int> nums={1,3,5,6};
+    vector<int> nums = {1, 3, 5, 6};
 
-    cout<<searchInsert(nums,5);
+    cout << searchInsert(nums, 5);
 }
